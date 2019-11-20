5a857944bbae6fc52827fe1e35529016c981795a
Jose Velarde <jvicius@gmail.com>
2019-11-20 16:01:26 -0600

    MVC Simple Login

--- MVCLogin/Controllers/HomeController.cs
+++ MVCLogin/Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MVCLogin.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,11 +23,38 @@ namespace MVCLogin.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult LogIn(string usuario, string password)
+        {
+            if (usuario == "admin" && password == "admin")
+            {
+                FormsAuthentication.SetAuthCookie(usuario, false);
+                return Json(1);
+            }
+            else
+            {
+                return Json(-1);
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+
     }
 }
